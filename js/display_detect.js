abe0061e0788d84c75896a2dd9421f2e49c9763c
Eric <ericcesena@outlook.com>
2015-11-10 13:19:57 -0800

    hope floats but i like flexing

--- js/display_detect.js
+++ js/display_detect.js
@@ -2,12 +2,18 @@
  * Created by Eric on 11/10/2015.
  */
 
+var max_tilt_gam = body.clientWidth
+var max_tilt_beta = body.clientHeight
+
 function handle_orentation(event){
-    var tilt_left = event.gamma;
-    var tilt_rigt = event.gamma;
-    if (tilt_left >= 90){
+
+    var tilt_left = event.alpha;
+    var tilt_right = event.beta;
+    if (tilt_left <= 90){
+        tilt_left = 90;
         alert('landscape left);
     }else if (tilt_right >= -90){
+        tilt_right = -90;
         alert('landscape right');
     }
 }
