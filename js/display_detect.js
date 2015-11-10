23240f9e71124c82f9a541fa6b118f2bd0310b1c
Eric <ericcesena@outlook.com>
2015-11-10 12:53:44 -0800

    updated to alert

--- js/display_detect.js
+++ js/display_detect.js
@@ -6,9 +6,9 @@ function handle_orentation(event){
     var tilt_left = event.gamma;
     var tilt_rigt = event.gamma;
     if (tilt_left >= 90){
-        console.log('landscape left);
+        alert('landscape left);
     }else if (tilt_right >= -90){
-        console.log('landscape right');
+        alert('landscape right');
     }
 }
 window.addEventListener('deviceorientation', handle_orentation());
