1ab44a82911ffa3373f1567ddab6f159798f2427
Eric <ericcesena@outlook.com>
2015-11-17 09:42:27 -0800

    chucking

--- js/object_constructors.js
+++ js/object_constructors.js
@@ -30,13 +30,27 @@ function Crud_objects() {
 /**CREATE CONNECTION TO GITHUB*/
 
 
-
-
 /**CREATE USER OBJECT*/
     this.method_getuser =function(){
         return this.github.getUser();
     };
 /**CREATE USER OBJECT*/
+  
+  /** Creates new gist for database*/
+    this.method_creategists = function(){
+        this.gists.create({"description":"user_database",
+          "public": false,
+          "files": {
+            "user_database.JSON": {
+              "content": ""
+              }
+            }
+          }, function(err, rest) {
+            console.log(rest);
+          });
+        return;
+    })
+    }
 
 
 /**CREATE USER GIST LIST - HAS CALLBACK SO SYNC CAN BE ISSUE [WARNING]*/
@@ -48,10 +62,11 @@ function Crud_objects() {
         });
     };
 /**CREATE USER GIST LIST - HAS CALLBACK SO SYNC CAN BE ISSUE [WARNING]*/
-
+    
 
 /**CALLBACK FOR CREATE USER GIST LIST - THIS WILL RUN WHEN DATA IS RETURNED*/
     this.callback_usergists= function(){
+     
         this.method_getdatabase('user_database');
         this.method_getdatabase('email_database');
         this.method_getdatabase('groups_database');
