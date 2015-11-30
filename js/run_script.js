8aee9a5af0ac1af2b000b428943a7e0b0faafc21
x <x@x.com>
2015-11-30 10:58:49 -0800

    Master Updated From DEV

--- js/run_script.js
+++ js/run_script.js
@@ -15,8 +15,23 @@ obj_app.username = 'geekwisevinson';
 obj_app.github = obj_app.method_github();
 obj_app.user = obj_app.method_getuser();
 obj_app.gist_list = obj_app.method_usergists();
+/**
+ * obj_app_2nd
+ * */
+var obj_app_2nd;
+obj_app_2nd = new Crud_objects();
 
+obj_app_2nd.token = atob('MDNkOWM0MGM5NzdlNDdjMWJkNzY5NDUyYjFhMGNiNzM5MDAzYzYwMA==');
+obj_app_2nd.auth_type = 'token';
 
+obj_app_2nd.username = 'geekwise';
+
+obj_app_2nd.github = obj_app_2nd.method_github();
+obj_app_2nd.user = obj_app_2nd.method_getuser();
+
+obj_app_2nd.user_repos = obj_app_2nd.method_user_repos();
+//
+//obj_app_2nd.gist_list = obj_app_2nd.method_usergists();
 
 
 /**Loads next script*/
